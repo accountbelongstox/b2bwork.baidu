0538f73bd50d05d7d17b13bfacb704418943e687
accountbelongstox <accountbelongstox@163.com>
2022-07-21 16:38:17 +0800

    new amend

--- push_gitee_and_github.bat
+++ push_gitee_and_github.bat
@@ -6,7 +6,7 @@ git commit -m "new amend"
 rem git remote add gitee git@gitee.com:accountbelongstox/b2bwork.baidu.git
 ssh -T git@gitee.com
 git push -u gitee master
-rem git remote add github git@github.com:accountbelongstox/b2bwork.baidu.git
+rem git remote set-url github git@github.com:accountbelongstox/b2bwork.baidu.git
 git branch -M 'main'
 git commit -m "new amend"
 ssh -T git@github.com
